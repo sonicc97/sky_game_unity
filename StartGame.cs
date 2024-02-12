4d5dda5479d1c85edb8ab84fd2a66a0e89789b59
Sonic <142159825+sonicc97@users.noreply.github.com>
2024-02-12 21:34:02 +0100

    Add files via upload

---
+++ StartGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+}
